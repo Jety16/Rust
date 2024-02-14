23650cec73e085666c553b549f02cc20b586e6b9
juan-dev <juan-dev@juan-dev.juan-dev>
2024-02-14 20:29:11 -0300

    errors and ? operand :)

---
+++ errors/target/debug/deps/errors-c277af05c7553abd.d
@@ -0,0 +1,5 @@
+/home/juan-dev/Escritorio/programing/rust/errors/target/debug/deps/errors-c277af05c7553abd: src/main.rs
+
+/home/juan-dev/Escritorio/programing/rust/errors/target/debug/deps/errors-c277af05c7553abd.d: src/main.rs
+
+src/main.rs:
---
+++ errors/target/debug/errors.d
@@ -0,0 +1,1 @@
+/home/juan-dev/Escritorio/programing/rust/errors/target/debug/errors: /home/juan-dev/Escritorio/programing/rust/errors/src/main.rs
