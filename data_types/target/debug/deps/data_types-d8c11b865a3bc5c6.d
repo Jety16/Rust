9fd3e5d163f98caaf25f28f04d442a21cf865958
juan-dev <juan-dev@juan-dev.juan-dev>
2024-02-18 19:16:03 -0300

    added data types with traits

---
+++ data_types/target/debug/deps/data_types-d8c11b865a3bc5c6.d
@@ -0,0 +1,5 @@
+/home/juan-dev/Escritorio/programing/rust/data_types/target/debug/deps/data_types-d8c11b865a3bc5c6: src/main.rs
+
+/home/juan-dev/Escritorio/programing/rust/data_types/target/debug/deps/data_types-d8c11b865a3bc5c6.d: src/main.rs
+
+src/main.rs:
--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
 }
